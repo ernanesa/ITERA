9e0d9d531166fa39b9c50ffde6682e8c3ce5f5d7
Ernane <ernane.sa@gmail.com>
2022-09-13 12:58:05 -0300

    chore: system configuration files

---
+++ Startup.cs
@@ -0,0 +1,62 @@
+using ITERA.Interfaces.Repositories;
+using ITERA.Interfaces.Services;
+using ITERA.Repositories;
+using ITERA.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace ITERA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ITERA", Version = "v1" });
+            });
+
+            services.AddScoped<IEmpresaService, EmpresaService>();
+            services.AddScoped<IGrupoService, GrupoService>();
+
+            services.AddScoped<IEmpresaRepository, EmpresaRepository>();
+            services.AddScoped<IGrupoRepository, GrupoRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ITERA v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
