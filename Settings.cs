68623a602890d43597782ca541f8ddfb6b9af507
Ernane <ernane.sa@gmail.com>
2022-09-13 15:26:25 -0300

    chore: configure authentication

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace ITERA
+{
+    public static class Settings
+    {
+        public static string Chave = "d41d8cd98f00b204e9800998ecf8427e";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,15 @@
+using System.Text;
 using ITERA.Interfaces.Repositories;
 using ITERA.Interfaces.Services;
 using ITERA.Repositories;
 using ITERA.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace ITERA
@@ -28,13 +31,57 @@ namespace ITERA
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ITERA", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            new string[]{}
+                        }
+                    }
+                );
             });
 
+            services.AddScoped<IUsuarioService, UsuarioService>();
             services.AddScoped<IEmpresaService, EmpresaService>();
             services.AddScoped<IGrupoService, GrupoService>();
 
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IEmpresaRepository, EmpresaRepository>();
             services.AddScoped<IGrupoRepository, GrupoRepository>();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Chave);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +98,7 @@ namespace ITERA
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
